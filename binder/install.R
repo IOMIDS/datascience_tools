9e1c599897b3ed8f6c6df83ee8351112d1446eb2
IOMIDS <56037512+IOMIDS@users.noreply.github.com>
2020-04-26 17:24:35 +0200

    Update to install.R to include packages for text mining

--- binder/install.R
+++ binder/install.R
@@ -1,1 +1,1 @@
-install.packages(c('tidyverse', 'stringr', 'pROC', 'randomForest', 'xgboost', 'ggplot2', 'wordcloud', 'readr', 'e1071', 'caret', 'partykit', 'rmarkdown'))
+install.packages(c('tidyverse', 'stringr', 'pROC', 'randomForest', 'xgboost', 'ggplot2', 'wordcloud', 'readr', 'e1071', 'caret', 'partykit', 'rmarkdown', 'tidytext', 'stopwords', 'tidyRSS', 'rvest'))
