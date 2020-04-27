276f45c1be03d19a7d62545bb31a47e416b4d8c7
IOMIDS <56037512+IOMIDS@users.noreply.github.com>
2020-04-27 15:40:39 +0200

    Reverting install.R to previous version

--- binder/install.R
+++ binder/install.R
@@ -1,2 +1,1 @@
-install.packages(c('tidyverse', 'stringr', 'pROC', 'randomForest', 'xgboost', 'ggplot2', 'wordcloud', 'readr', 'e1071', 'caret', 'partykit', 'rmarkdown', 'tidytext', 'stopwords', 'rvest', 'devtools'))
-devtools::install_github('robertmyles/tidyrss')
+install.packages(c('tidyverse', 'stringr', 'pROC', 'randomForest', 'xgboost', 'ggplot2', 'wordcloud', 'readr', 'e1071', 'caret', 'partykit', 'rmarkdown', 'tidytext', 'stopwords', 'rvest'))
