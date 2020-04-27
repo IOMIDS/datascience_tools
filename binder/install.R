98572d7875d9e123c80cff5bfb2759e8b6fbd9f4
IOMIDS <56037512+IOMIDS@users.noreply.github.com>
2020-04-27 08:39:58 +0200

    Update install.R to include tidyRSS via devtools install from github

--- binder/install.R
+++ binder/install.R
@@ -1,1 +1,2 @@
-install.packages(c('tidyverse', 'stringr', 'pROC', 'randomForest', 'xgboost', 'ggplot2', 'wordcloud', 'readr', 'e1071', 'caret', 'partykit', 'rmarkdown', 'tidytext', 'stopwords', 'tidyRSS', 'rvest'))
+install.packages(c('tidyverse', 'stringr', 'pROC', 'randomForest', 'xgboost', 'ggplot2', 'wordcloud', 'readr', 'e1071', 'caret', 'partykit', 'rmarkdown', 'tidytext', 'stopwords', 'rvest', 'devtools'))
+devtools::install_github('robertmyles/tidyrss')
